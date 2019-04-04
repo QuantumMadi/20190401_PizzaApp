7efb519e85acf4bffde1ab737f2d986627b1529b
Madi <45874448+QuantumMadi@users.noreply.github.com>
2019-04-04 11:48:49 +0600

    Reflection, parametrs, transaction for common add method

--- PizzaApp/PizzaAppDataAccess/UsersTableDataService.cs
+++ PizzaApp/PizzaAppDataAccess/UsersTableDataService.cs
@@ -10,23 +10,44 @@ namespace PizzaAppDataAccess
 {
     public class UsersTableDataService<T>
     {
+
         private readonly string _connectionString;
         public UsersTableDataService()
         {
             _connectionString = $@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\АбдигалиевМ.CORP.000\source\repos\20190401_PizzaApp\PizzaApp\PizzaAppDataAccess\PizzaApp.mdf;Integrated Security=True";
         }
+        private static void CreateParametr()
+        {
+            
+        }
+
         public void Add(T item)
         {
             Type type = typeof(T);
-            type.GetFields();
+            PropertyInfo[] fieldInfo = type.GetProperties();
 
             using (var connection = new SqlConnection(_connectionString))
             using (var command = connection.CreateCommand())
             {
                 try
                 {
-                    connection.Open();
-                    command.CommandText = $"insert into Users values('{user.Login}', '{user.Password}')";
+                    connection.Open();                 
+                    StringBuilder commandString = new StringBuilder($"insert into {type.Name}s values(");
+
+                    for(int i = 0; i < fieldInfo.Length; ++i)
+                    {
+                        if(i != fieldInfo.Length - 1)
+                        {
+                            commandString.Append($"@{fieldInfo[i]}");
+                            commandString.Append(',');
+                        }
+                        else
+                        {
+                            commandString.Append($"@{fieldInfo[i]}");
+                            commandString.Append(')');
+                        }
+                    }
+                                      
                     var affectedRows = command.ExecuteNonQuery();
                     if (affectedRows < 1) throw new Exception("No rows affected");
                 }
